f0d85166ecf26d5d1b699ee2b383d73cfad60943
Subzeero <76539691+Subzeero@users.noreply.github.com>
2024-01-01 12:21:58 -0600

    Write server - model
    * Link everything together with the model.
    * Initialize database & express backend

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const { Database: db } = require("./db");
+const routes = require("./routes");
+
+async function run() {
+  console.log("Starting DB...");
+  await db.init();
+
+  const app = express();
+  app.use(cors({ origin: "http://locahost:3000" }));
+  app.use("/", routes);
+
+  app.listen(4000);
+  console.log("Backend Running!");
+}
+
+run();
